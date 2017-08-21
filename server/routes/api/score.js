dbe59cfa3ed4830209c2620b422c85c0b2dc9f64
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-08-21 15:45:54 -0700

    Score post change for new client side form

--- server/routes/api/score.js
+++ server/routes/api/score.js
@@ -25,7 +25,7 @@ module.exports = (app) => {
 	});
 	
 	app.post('/api/score', function (req, res, next) {
-		console.log(req.body.name);
+		console.log(req.body);
 		const score = new Score({name: req.body.name, score: req.body.score});
 		score.save()
 			.then(newScore => {
